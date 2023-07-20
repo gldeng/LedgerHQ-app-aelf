ce8d4397cc3515004c595bf140a48402d975989b
Guilane DENGUIR <83951892+GuilaneDen@users.noreply.github.com>
2023-07-20 13:03:38 +0000

    fix(address): added the missed part

--- lib/include/aelf/printer.h
+++ lib/include/aelf/printer.h
@@ -7,6 +7,7 @@
 #define SUMMARY_LENGTH       7
 #define TITLE_SIZE           32
 #define BASE58_PUBKEY_LENGTH 45
+#define BASE58_CHECK_PUBKEY_LENGTH 49
 #define BASE58_PUBKEY_SHORT  (SUMMARY_LENGTH + 2 + SUMMARY_LENGTH + 1)
 
 int print_token_amount(uint64_t amount,
--- lib/transaction_summary.c
+++ lib/transaction_summary.c
@@ -155,7 +155,7 @@ static int transaction_summary_update_display_for_item(const SummaryItem* item,
             BAIL_IF(print_u64(item->u64, G_transaction_summary_text, TEXT_BUFFER_LENGTH));
             break;
         case SummaryItemPubkey: {
-            char tmp_buf[BASE58_PUBKEY_LENGTH];
+            char tmp_buf[BASE58_CHECK_PUBKEY_LENGTH];
             BAIL_IF(encode_base58_check(item->pubkey, PUBKEY_SIZE, tmp_buf, sizeof(tmp_buf)));
             if (flags & DisplayFlagLongPubkeys) {
                 BAIL_IF(print_string(tmp_buf, G_transaction_summary_text, TEXT_BUFFER_LENGTH));
